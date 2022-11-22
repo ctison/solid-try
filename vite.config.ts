4b188c864286bd02681d70d631efa705d830a5e6
Charles Tison <ctison@pm.me>
2022-11-22 17:00:11 +0100

    chore: setup vercel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 import windicss from 'vite-plugin-windicss'
+import vercel from 'solid-start-vercel'
 
 export default defineConfig({
-  plugins: [windicss(), solid()],
+  plugins: [
+    windicss(),
+    solid({
+      adapter: vercel({
+        edge: true,
+      }),
+    }),
+  ],
 })
