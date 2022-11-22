6027360f21e18af42c5eff55d54164182dc2bdbc
Charles Tison <ctison@pm.me>
2022-11-22 17:17:10 +0100

    chore: setup netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 import windicss from 'vite-plugin-windicss'
-import vercel from 'solid-start-vercel'
+import netlify from 'solid-start-netlify'
 
 export default defineConfig({
   plugins: [
     windicss(),
     solid({
-      adapter: vercel({
+      adapter: netlify({
         edge: true,
       }),
     }),
